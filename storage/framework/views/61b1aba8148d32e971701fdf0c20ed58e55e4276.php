fef0f77dad2a3eaf389a0436b498850316788db5
Abrar Ajaz Wani <abhaywani114@gmail.com>
2020-12-05 21:53:36 +0530

    View removed

--- storage/framework/views/61b1aba8148d32e971701fdf0c20ed58e55e4276.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-<p>We have successfully resetted your password. <br/>Your new password is: <strong><?php echo e($password); ?></strong></p>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('email.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Server_Files\Network Venture\project_7001a\resources\views/email/reset_password.blade.php ENDPATH**/ ?>
