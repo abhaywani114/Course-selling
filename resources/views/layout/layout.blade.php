90f5239cc78ccb799973d6a201ac8d6836df270c
Abrar Ajaz <34162375+abhaywani114@users.noreply.github.com>
2020-12-06 10:37:16 +0530

    Update layout.blade.php

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -224,7 +224,7 @@
 				<li class="nav-item dropdown">
       				  <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" 
 						role="button" data-toggle="dropdown" aria-haspopup="true" 
-						aria-expanded="false">User</a>
+						aria-expanded="false">{{Auth::User()->name}}</a>
 					<div class="dropdown-menu" aria-labelledby="adminDropdown">
 					  <a class="dropdown-item" href="{{route('user.my_courses')}}">My Courses</a>
 					  <a class="dropdown-item" href="{{route('myprofile.update')}}">My Settings</a>
